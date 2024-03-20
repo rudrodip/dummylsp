eec84eff4f041cd74ecb97cc6e0431763d6a9d98
rudrodip <official.rudrodipsarker@gmail.com>
2024-03-20 15:37:07 +0600

    Add new types for text document handling

--- lsp/textdocument.go
+++ lsp/textdocument.go
@@ -6,3 +6,16 @@ type TextDocumentItem struct {
 	Version    int    `json:"version"`
 	Text       string `json:"text"`
 }
+
+type TextDocumentIdentifier struct {
+	URI string `json:"uri"`
+}
+
+type VersionTextDocumentIdentifier struct {
+	TextDocumentIdentifier
+	Version int `json:"version"`
+}
+
+type TextDocumentContentChangeEvent struct {
+	Text string `json:"text"`
+}
