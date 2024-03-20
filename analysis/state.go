50f2a79b338ff932c6968e27d5049e82b942f02e
rudrodip <official.rudrodipsarker@gmail.com>
2024-03-20 15:36:53 +0600

    Add State struct and methods for managing documents

---
+++ analysis/state.go
@@ -0,0 +1,20 @@
+package analysis
+
+type State struct {
+	// file names to contents
+	Documents map[string]string
+}
+
+func NewState() State {
+	return State{
+		Documents: make(map[string]string),
+	}
+}
+
+func (s *State) OpenDocument(uri, text string) {
+	s.Documents[uri] = text
+}
+
+func (s *State) UpdateDocument(uri, text string) {
+	s.Documents[uri] = text
+}
