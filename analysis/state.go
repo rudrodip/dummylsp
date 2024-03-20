d27988fd67ff1396274f1ded918d38931a1241e6
rudrodip <official.rudrodipsarker@gmail.com>
2024-03-20 16:41:35 +0600

    Add Hover functionality to State struct

--- analysis/state.go
+++ analysis/state.go
@@ -1,5 +1,11 @@
 package analysis
 
+import (
+	"fmt"
+
+	"github.com/rudrodip/dummylsp/lsp"
+)
+
 type State struct {
 	// file names to contents
 	Documents map[string]string
@@ -17,4 +23,18 @@ func (s *State) OpenDocument(uri, text string) {
 
 func (s *State) UpdateDocument(uri, text string) {
 	s.Documents[uri] = text
+}
+
+func (s *State) Hover(id int, uri string, position lsp.Position) lsp.HoverResponse {
+	document := s.Documents[uri]
+
+	return lsp.HoverResponse{
+		Response: lsp.Response{
+			RPC: "2.0",
+			ID:  &id,
+		},
+		Result: lsp.HoverResult{
+			Contents: fmt.Sprintf("File %s characters %d", uri, len(document)),
+		},
+	}
 }
