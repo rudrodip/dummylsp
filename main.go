0064c22f8b87e0bcd33edf8684dda89b1173fdec
rudrodip <official.rudrodipsarker@gmail.com>
2024-03-20 11:14:02 +0600

    Refactor main.go to add logging and message handling

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"log"
+	"os"
+
+	"github.com/rudrodip/dummylsp/rpc"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	logger := getLogger("dummylsp.log")
+	logger.Println("Starting dummylsp")
+
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(rpc.Split)
+
+	for scanner.Scan() {
+		msg := scanner.Text()
+		handleMessage(logger, msg)
+	}
+}
+
+func handleMessage(logger *log.Logger, msg any) {
+	logger.Println(msg)
+}
+
+func getLogger(filename string) *log.Logger {
+	logFile, err := os.OpenFile(filename, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 06666)
+	if err != nil {
+		panic(err)
+	}
+
+	return log.New(logFile, "[dummylsp]", log.Ldate|log.Ltime|log.Lshortfile)
 }
