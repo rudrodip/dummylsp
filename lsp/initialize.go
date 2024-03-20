bb1661e3360082bd229097cf9e999d0e054a0412
rudrodip <official.rudrodipsarker@gmail.com>
2024-03-20 11:48:10 +0600

    Add InitializeResponse struct and NewInitializeResponse function

--- lsp/initialize.go
+++ lsp/initialize.go
@@ -13,3 +13,36 @@ type ClientInfo struct {
 	Name    string `json:"name"`
 	Version string `json:"version"`
 }
+
+type InitializeResponse struct {
+	Response
+	Result InitializeResult `json:"result"`
+}
+
+type InitializeResult struct {
+	Capabilities ServerCapabilities `json:"capabilities"`
+	ServerInfo   *ServerInfo        `json:"serverInfo,omitempty"`
+}
+
+type ServerCapabilities struct{}
+
+type ServerInfo struct {
+	Name    string `json:"name"`
+	Version string `json:"version"`
+}
+
+func NewInitializeResponse(id int) InitializeResponse {
+	return InitializeResponse{
+		Response: Response{
+			ID:  &id,
+			RPC: "2.0",
+		},
+		Result: InitializeResult{
+			Capabilities: ServerCapabilities{},
+			ServerInfo: &ServerInfo{
+				Name:    "dummylsp",
+				Version: "0.0.1-alpha",
+			},
+		},
+	}
+}
